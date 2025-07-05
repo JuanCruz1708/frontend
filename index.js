6450c2f2f34f0557dd5b4c6cb38727064a8c264f
JuanCruz1708 <juan_cuasolo@hotmail.com>
2025-07-05 19:01:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
